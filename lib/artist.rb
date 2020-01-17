d47dab291d5e59307a45bbe26ca12140fd6b005d
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-01-17 12:28:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,27 +1,31 @@
+class Artist
 
-class Artist 
-  attr_accessor :name
+  attr_accessor :name, :songs
+
+  @@song_count = 0
+
   def initialize(name)
-    @name =  name 
-  end 
-  def songs 
-    Song.all.select do |song|
-      song.artist == self 
-  end 
-end 
-def add_song(song)
- song.artist = self 
-end 
-def add_song_by_name(song_name)
- 
-singer=Song.new(song_name)
-add_song(singer)
-end 
-def self.song_count
-  Song.all.length
-end 
-end 
+    @name = name
+    @songs = []
+  end
 
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
 
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def self.song_count
+    @@song_count
+  end
+
+end
   
   
