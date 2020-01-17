b7f0306b56b1c3bbcf98f3bcd7632c878f918cc7
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-01-17 12:29:15 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,26 +1,28 @@
+class Author
 
-class Author 
-  attr_accessor :name
+  attr_accessor :name, :posts
+
+  @@post_count = 0
+  
   def initialize(name)
-    @name =  name 
-  end 
- 
-  def posts 
-    Post.all.select do |title|
-      title.author == self 
-  end 
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+
+  def self.post_count
+    @@post_count
+  end
 end
-
-def add_post(title)
-title.author = self 
-end 
-def add_post_by_title(title)
-  poster=Post.new(title)
-  add_post(poster)
-end
-
-def self.post_count
-  Post.all.length
-end 
-end 
-
