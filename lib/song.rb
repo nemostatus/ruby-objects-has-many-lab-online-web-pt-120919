ba9bcaa1ecc9b1efd3015dc8c685c1d7960e6f3c
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-01-17 12:34:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,18 @@
-class Song 
-  attr_accessor :artist,:name 
+class Song
+  attr_accessor :name, :artist
+
   @@all = []
+
   def initialize(name)
-    @name = name 
-    @@all << self 
-  end 
-  def self.all 
-    @@all 
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    artist.name if artist
   end
 end
